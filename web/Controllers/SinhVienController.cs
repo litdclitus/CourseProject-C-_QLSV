581cd6dc6e55c6d25f3098990b3f331374a65c86
litdclitus <dangnh799@gmail.com>
2024-10-07 18:50:29 +0700

    re-update course project C#

---
+++ web/Controllers/SinhVienController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace firstWeb.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /*
+        public string XemDiem() {
+            return "Đây là  trang web xem điểm sinh viên";
+        }*/
+
+       /* public string XemDiem(string name, double diem=9)
+        {
+            return HtmlEncoder.Default.Encode($"Ten {name} Diem trung binh {diem}");  
+        }*/
+        /*
+        public string XemDiem(string name, double diem=9, int ID=1)
+        {
+            return HtmlEncoder.Default.Encode($"ID {ID} Ten {name} Diem trung binh {diem}");  
+        }*/
+        public IActionResult XemDiem(string name, double diem = 9)
+        {
+            ViewData["name"] = name;
+            ViewData["diem"] = diem;
+            return View();
+        }
+        
+
+    }
+}
---
+++ web/Models/StoreContext.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+namespace firstWeb.Models
+{
+    public class StoreContext
+    {
+        public string ConnectionString { get; set; }//biết thành viên 
+
+        public StoreContext(string connectionString) //phuong thuc khoi tao
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection() //lấy connection 
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+        //lấy danh sách các khoa
+        public List<Khoa> GetKhoas()
+        {
+            List<Khoa> list = new List<Khoa>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                string str = "select * from KHOA";
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Khoa()
+                        {
+                            MaKhoa = reader["MaKhoa"].ToString(),
+                            TenKhoa = reader["TenKhoa"].ToString(),
+                        });
+                    }
+                    reader.Close(); 
+                }
+                
+                conn.Close();
+                
+            }
+            return list;
+        }
+        public int InsertKhoa(Khoa kh)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                var str = "insert into KHOA values(@makhoa, @tenkhoa)";
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+                cmd.Parameters.AddWithValue("makhoa", kh.MaKhoa);
+                cmd.Parameters.AddWithValue("tenkhoa", kh.TenKhoa);
+                return (cmd.ExecuteNonQuery());
+
+            }
+        }
+        public int UpdateKhoa(Khoa kh)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                var str = "update KHOA set TenKhoa = @tenkhoa where MaKhoa=@makhoa";
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+                cmd.Parameters.AddWithValue("tenkhoa", kh.TenKhoa);
+                cmd.Parameters.AddWithValue("makhoa", kh.MaKhoa);
+                return (cmd.ExecuteNonQuery());
+            }
+        }
+        
+        public int XoaKhoa(string Id)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                var str = "delete from KHOA where MaKhoa=@makhoa";
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+                cmd.Parameters.AddWithValue("makhoa", Id);
+                return (cmd.ExecuteNonQuery());
+            }
+        }
+        public int InsertBoMon(BoMon bm)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                var str = "insert into BOMON values(@mabomon, @tenbomon,@makhoa)";
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+                cmd.Parameters.AddWithValue("mabomon", bm.MaBoMon);
+                cmd.Parameters.AddWithValue("tenbomon", bm.TenBoMon);
+                cmd.Parameters.AddWithValue("makhoa", bm.MaKhoa);
+                return (cmd.ExecuteNonQuery());
+
+            }
+        }
+        public Khoa ViewKhoa(string Id)
+        {
+            //Khoa kh = new Khoa("MK01","HTTT");
+            Khoa kh = new Khoa();
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                var str = "select * from KHOA where MaKhoa=@makhoa";
+                MySqlCommand cmd = new MySqlCommand(str, conn);
+                cmd.Parameters.AddWithValue("makhoa", Id);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    kh.MaKhoa=reader["MaKhoa"].ToString();
+                    kh.TenKhoa = reader["TenKhoa"].ToString();
+                }
+            }
+            return (kh);
+        }
+    
+        public StoreContext()
+        {
+        }
+    }
+}
